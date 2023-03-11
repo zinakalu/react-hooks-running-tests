b8828ec99fc1d57f51e0ea54fa313c7b4de07318
zinakalu <zina.kalu@gmail.com>
2023-03-11 09:29:12 -0500

    react running tests

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,8 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  screen.debug();
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
+
